c76f0796bbe018d1d77da98881df60d9635e479a
a lan <bluenowte97@gmail.com>
2018-11-04 00:31:28 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  # puts "Hi, my name is #{name}."
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end
